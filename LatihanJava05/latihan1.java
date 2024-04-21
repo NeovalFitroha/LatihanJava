46dc6ed3cdf03f71ac6638d53f8cc66fb445cd25
unknown <neovalfitroha23@gmail.com>
2024-04-21 16:09:52 +0700

    Latihan Java

---
+++ LatihanJava05/latihan1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MembuatForm;
+import javax.swing.*;
+/**
+ *
+ * @author LENOVO
+ */
+class latihan1 {
+    
+    public static void main(String[]args){
+        JFrame myWindow;
+        
+        myWindow = new JFrame();//Pembuatan Projek
+        myWindow.setSize(300,200);//Setting Ukuran Form
+        myWindow.setTitle("My First Java Program");// Judul untuk form
+        myWindow.setVisible(true);//Untuk menampilkan Form
+    }
+}
